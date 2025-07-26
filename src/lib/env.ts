05b450896f8e364245c9c3f8f08b94ef829c9b08
ivanmeda993 <ivanmilicevicmeda@gmail.com>
2025-07-26 15:29:59 +0200

    Feat(deps): Updates dependencies and adds env-nextjs
    Updates dependencies in bun.lock and package.json to include "@t3-oss/env-nextjs" and recharts, and removes unused packages.
    
    Introduces environment variable validation using "@t3-oss/env-nextjs" to ensure type safety and prevent runtime errors.
    
    Adds card component to the UI library, providing a consistent visual style for cards.

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,27 +1,31 @@
-// lib/env.ts
+import { createEnv } from '@t3-oss/env-nextjs';
 import { z } from 'zod';
 
-const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'test', 'production']),
-  DATABASE_URL: z.url(),
-  BETTER_AUTH_SECRET: z
-    .string()
-    .min(10, 'BETTER_AUTH_SECRET must be at least 10 characters long'),
-  BETTER_AUTH_URL: z.url('BETTER_AUTH_URL must be a valid URL'),
-  GITHUB_CLIENT_ID: z.string(),
-  GITHUB_CLIENT_SECRET: z.string(),
-  GOOGLE_CLIENT_ID: z.string(),
-  GOOGLE_CLIENT_SECRET: z.string(),
+export const ENV = createEnv({
+  server: {
+    NODE_ENV: z
+      .enum(['development', 'production', 'test'])
+      .default('development'),
+    DATABASE_URL: z.url().min(1, 'DATABASE_URL is required'),
+    BETTER_AUTH_SECRET: z.string().min(1, 'BETTER_AUTH_SECRET is required'),
+    BETTER_AUTH_URL: z.url().min(1, 'BETTER_AUTH_URL is required'),
+    GITHUB_CLIENT_ID: z.string().min(1, 'GITHUB_CLIENT_ID is required'),
+    GITHUB_CLIENT_SECRET: z.string().min(1, 'GITHUB_CLIENT_SECRET is required'),
+    GOOGLE_CLIENT_ID: z.string().min(1, 'GOOGLE_CLIENT_ID is required'),
+    GOOGLE_CLIENT_SECRET: z.string().min(1, 'GOOGLE_CLIENT_SECRET is required'),
+  },
+  client: {
+    NEXT_PUBLIC_URL: z.url().min(1, 'NEXT_PUBLIC_URL is required'),
+  },
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    NODE_ENV: process.env.NODE_ENV,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL,
+  },
 });
-
-const parsedEnv = envSchema.safeParse(process.env);
-
-if (!parsedEnv.success) {
-  console.error(
-    '❌ Invalid environment variables:',
-    z.flattenError(parsedEnv.error),
-  );
-  throw new Error('Invalid environment variables');
-}
-
-export const ENV = parsedEnv.data;
